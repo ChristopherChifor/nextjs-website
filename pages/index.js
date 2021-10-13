d1ce326a9d471ad11368a9e7342cc90a87295c51
chrischifor <christopher_chifor@yahoo.ca>
2021-10-13 19:50:12 -0400

    Added Clicky support for tracking

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import App from '../stuff/App'
 import styles from '../styles/Home.module.css'
-import{ init } from 'emailjs-com';
+import { init } from 'emailjs-com';
 init("user_fNzCzFsGtx3gCQ0ErLfZp");
 
 export default function Home() {
@@ -10,8 +10,12 @@ export default function Home() {
       <Head>
         <title>Christopher Chifor</title>
         <link rel="icon" href="/favicon.png" />
+
       </Head>
-      <App/>
+      <App />
+      <a title="Real Time Web Analytics" href="http://clicky.com/101336900"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+      <script async src="//static.getclicky.com/101336900.js"></script>
+      <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101336900ns.gif" /></p></noscript>
     </div>
   )
 }
