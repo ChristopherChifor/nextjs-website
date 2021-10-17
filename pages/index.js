5dcbb634c4254e52be195e251b1307f7a344bc38
chrischifor <christopher_chifor@yahoo.ca>
2021-10-17 12:55:49 -0400

    edits with analytics

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,9 @@ export default function Home() {
       <Head>
         <title>Christopher Chifor</title>
         <link rel="icon" href="/favicon.png" />
-
       </Head>
       <App />
-      <a title="Real Time Web Analytics" href="http://clicky.com/101336900"><img alt="Clicky" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
-      <script async src="//static.getclicky.com/101336900.js"></script>
-      <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101336900ns.gif" /></p></noscript>
+
     </div>
   )
 }
