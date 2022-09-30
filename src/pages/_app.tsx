fca35f8c17aa48157ed8ce04339e3ac684aa0799
ChristopherChifor <christopher_chifor@yahoo.ca>
2022-09-30 15:40:46 -0400

    feat: updated description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>NextJS TW</title>
+        <title>Chris Chifor</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Personal site of Chris Chifor, a software engineer based in Toronto, Canada."
+        />
       </Head>
       <Component {...pageProps} />
     </>
