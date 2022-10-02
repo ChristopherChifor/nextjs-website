52a572f67b579c8fd71695518725326866803dd5
ChristopherChifor <christopher_chifor@yahoo.ca>
2022-10-01 21:33:59 -0400

    linkedin added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -103,7 +103,7 @@ export default function Home() {
               />
             </a>
             <a
-              href="https://github.com/ChristopherChifor"
+              href="https://www.linkedin.com/in/christopher-chifor"
               target="_blank"
               rel="noopener noreferrer"
             >
