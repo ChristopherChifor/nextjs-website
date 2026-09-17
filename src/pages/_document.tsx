26c7a3c4ec95df48fec9640c175781f0a3d4597a
ChristopherChifor <christopher_chifor@yahoo.ca>
2026-09-17 08:50:24 -0400

    remove icon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3C/svg%3E" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
